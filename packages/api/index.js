9a645f4836ba4d605de99ff8baccfb9c2b2cef12
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-11-23 14:21:49 +0100

    Delete / after corsOptions origin

--- packages/api/index.js
+++ packages/api/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const corsOptions = {
   // origin: '*',
-  origin: 'https://bluejay-helpdesk.herokuapp.com/',
+  origin: 'https://bluejay-helpdesk.herokuapp.com',
   // origin: ['http://localhost:3000', 'http://localhost:19006'], // TODO: how to allow the sandbox access?
   // origin: 'http://localhost:19000',
   credentials: true,
