df6a9b6fd1b55c4d348a84fdfaa734446ac1d5c1
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-11-24 15:15:17 +0100

    Add more console.log s to employee session resolver

--- packages/api/utils/resolvers.js
+++ packages/api/utils/resolvers.js
@@ -194,6 +194,9 @@ exports.resolvers = {
     employeeSession: async (parent, args, context) => {
       // context.req.header.cookie only includes the sessionCookie (parsed already via next.js)
       console.log('context: ', context);
+      console.log('context.req: ', context.req);
+      console.log('context.req.headers: ', context.req.headers);
+      console.log('context.req.headers.');
       const sessionCookie = context.req.headers.cookie;
       console.log('sessionCookie: ', sessionCookie);
       const validationResult = await getValidEmployeeSessionByToken(
