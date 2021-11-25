d8e440721c2aedf623aff9e02104b82198ca5080
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-11-25 09:43:59 +0100

    Add expo link to corsOptions

--- packages/api/index.js
+++ packages/api/index.js
@@ -18,6 +18,7 @@ const corsOptions = {
   origin: [
     'https://bluejay-helpdesk.herokuapp.com',
     'http://192.168.0.115:19006',
+    'exp://exp.host/@lorenz-arthur/bluejay-premium-app',
     'exp://192.168.0.115:19006',
   ],
   // origin: ['http://localhost:3000', 'http://localhost:19006'], // TODO: how to allow the sandbox access?
