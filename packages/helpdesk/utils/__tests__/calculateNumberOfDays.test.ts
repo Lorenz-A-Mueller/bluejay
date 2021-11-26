e03f77fadccd5c6ea91ee671f0c19bf74a484a34
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-11-26 11:26:56 +0100

    Correct jest test in helpdesk, include .github/workflows folder with jest test file

--- packages/helpdesk/utils/__tests__/calculateNumberOfDays.test.ts
+++ packages/helpdesk/utils/__tests__/calculateNumberOfDays.test.ts
@@ -1,5 +1,5 @@
 import { calculateNumberOfDays } from '../calculateNumberOfDays';
 
-test('Test whether the number of days between 01-01-2020 and 01-01-2021 is calculated to be 365 ', () => {
-  expect(calculateNumberOfDays(1577836800000, 1609459200000)).toBe(365);
+test('Test whether the number of days between 01-01-2020 and 01-01-2021 is calculated to be 366 ', () => {
+  expect(calculateNumberOfDays(1577836800000, 1609459200000)).toBe(366);
 });
